2a8bda3ab8f4b3b00fc6fae7f0558ac515ace32b
ThanhLe <109021245+hocsinhkha@users.noreply.github.com>
2025-07-14 20:07:10 +0700

    Update db.php

--- core/db.php
+++ core/db.php
@@ -1,3 +1,11 @@
 <?php
-$db = new PDO('sqlite:' . __DIR__ . '/../database/database.db');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $db = new PDO(
+        "pgsql:host=dpg-d1qfuvbe5dus73e7gdug-a;port=5432;dbname=toolfb",
+        "toolfb_user",
+        "uOhJ10P9CQd2d6O3b0UqpwZyCxYZZXJg"
+    );
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("❌ Kết nối database thất bại: " . $e->getMessage());
+}
