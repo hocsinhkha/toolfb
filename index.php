6db4e1240bbe923dcef3edb895923503ba9330b4
ThanhLe <109021245+hocsinhkha@users.noreply.github.com>
2025-07-14 15:47:26 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Lọc Bạn Bè Facebook</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+  <h2>Lọc Bạn Bè Facebook</h2>
+  <form method="post" action="get_friends.php">
+    <textarea name="cookie" rows="4" cols="50" placeholder="Dán cookie Facebook..."></textarea><br>
+    <button type="submit">Lọc</button>
+  </form>
+</body>
+</html>
